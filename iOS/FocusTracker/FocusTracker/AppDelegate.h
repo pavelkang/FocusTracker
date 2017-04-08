f135aa032d35fbd82c81ae1d8d709523aa51bec2
Ted Li <tedwxli@gmail.com>
2017-04-08 14:53:21 -0400

    iOS facial detection

---
+++ iOS/FocusTracker/FocusTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FocusTracker
+//
+//  Created by Ted Li on 4/8/17.
+//  Copyright © 2017 Ted Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/FocusTracker/FocusTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FocusTracker
+//
+//  Created by Ted Li on 4/8/17.
+//  Copyright © 2017 Ted Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
